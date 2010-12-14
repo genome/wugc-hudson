2afa63c447d70bfa0fd8da294b768eaea0c8a096
Nathan Nutter <nnutter@genome.wustl.edu>
2010-12-14 15:05:44 -0600

    refactoring code for changes in location of scripts, etc.

--- lib/Defaults.pm
+++ lib/Defaults.pm
@@ -1,10 +1,11 @@
 package Defaults;
 
 our $RSS_FEED_URL = 'http://hudson:8090/job/Genome/rssAll';
-our $BUILD_PATH = '/gscuser/jlolofie/.hudson/jobs/Genome/builds';
+our $BUILD_PATH = '/gsc/scripts/opt/genome/snapshots/unstable';
+our $TESTED_PATH = '/gsc/scripts/opt/genome/snapshots/tested';
+our $STABLE_PATH = '/gsc/scripts/opt/genome/snapshots/stable';
 our $SNAPSHOT_PATH = $ENV{HOME} . '/.hudson_snapshot';
-our $GSCPAN = $ENV{GSCPAN} || 'svn+ssh://svn/srv/svn/gscpan';
 
 our $UR_REPOSITORY = 'git://github.com/sakoht/UR.git';
-our $WORKFLOW_REPOSITORY = 'ssh://git/srv/git/workflow.git';
-our $GENOME_REPOSITORY = 'ssh://git/srv/git/genome.git';
+our $WORKFLOW_REPOSITORY = 'ssh://git.gsc.wustl.edu/srv/git/workflow.git';
+our $GENOME_REPOSITORY = 'ssh://git.gsc.wustl.edu/srv/git/genome.git';
