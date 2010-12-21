59f7da6481c1874e7392da4b146fb693ce8af25d
Nathan Nutter <nnutter@genome.wustl.edu>
2010-12-21 17:31:10 -0600

    update to use current/symlink instead of current-symlink

--- lib/Defaults.pm
+++ lib/Defaults.pm
@@ -9,9 +9,9 @@ our $BUILD_PATH = $BASE_DIR . '/snapshots/unstable';
 our $UNSTABLE_PATH = $BUILD_PATH;
 our $TESTED_PATH = $BASE_DIR . '/snapshots/tested';
 our $STABLE_PATH = $BASE_DIR . '/snapshots/stable';
-our $STABLE_PIPELINE = $BASE_DIR . '/current-stable';
-our $STABLE_WEB = $BASE_DIR . '/current-web';
-our $STABLE_USER = $BASE_DIR . '/current-tested';
+our $STABLE_PIPELINE = $BASE_DIR . '/current/stable';
+our $STABLE_WEB = $BASE_DIR . '/current/web';
+our $STABLE_USER = $BASE_DIR . '/current/tested';
 our $SNAPSHOT_PATH = $ENV{HOME} . '/.hudson_snapshot';
 
 our $UR_REPOSITORY = 'git://github.com/sakoht/UR.git';
