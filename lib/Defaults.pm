966a2f6a54114141cbe8fc89292d32e8f96498b8
Nathan Nutter <nnutter@genome.wustl.edu>
2010-12-29 10:07:06 -0600

    prettified layout

--- lib/Defaults.pm
+++ lib/Defaults.pm
@@ -3,19 +3,25 @@ package Defaults;
 our $EMAIL = 'jlolofie@genome.wustl.edu';
 our $GIT_BIN = '/gsc/bin/git';
 our $RSS_FEED_URL = 'http://hudson.gsc.wustl.edu/job/Genome/rssAll';
-our $BASE_DIR = '/gsc/scripts/opt/genome';
-our $BIN_DIR = $BASE_DIR . '/bin';
-our $BUILD_PATH = $BASE_DIR . '/snapshots/unstable';
-our $UNSTABLE_PATH = $BUILD_PATH;
-our $TESTED_PATH = $BASE_DIR . '/snapshots/tested';
-our $STABLE_PATH = $BASE_DIR . '/snapshots/stable';
-our $CUSTOM_PATH = $BASE_DIR . '/snapshots/custom';
-our $OLD_PATH = $BASE_DIR . '/snapshots/old';
-our $CURRENT_PIPELINE = $BASE_DIR . '/current/pipeline';
-our $CURRENT_WEB = $BASE_DIR . '/current/web';
-our $CURRENT_USER = $BASE_DIR . '/current/user';
-our $SNAPSHOT_PATH = $ENV{HOME} . '/.hudson_snapshot';
-
+our
+our $BASE_DIR 			= '/gsc/scripts/opt/genome';
+our $BIN_DIR 			= $BASE_DIR . '/bin';
+our $UNSTABLE_PATH 		= $BASE_DIR . '/snapshots/unstable';
+our $OLD_PATH 			= $BASE_DIR . '/snapshots/old';
+our $TESTED_PATH 		= $BASE_DIR . '/snapshots/tested';
+our $STABLE_PATH 		= $BASE_DIR . '/snapshots/stable';
+our $CUSTOM_PATH 		= $BASE_DIR . '/snapshots/custom';
+our $CURRENT_PIPELINE	= $BASE_DIR . '/current/pipeline';
+our $CURRENT_WEB			= $BASE_DIR . '/current/web';
+our $CURRENT_USER		= $BASE_DIR . '/current/user';
+our
 our $UR_REPOSITORY = 'git://github.com/sakoht/UR.git';
 our $WORKFLOW_REPOSITORY = 'ssh://git.gsc.wustl.edu/srv/git/workflow.git';
 our $GENOME_REPOSITORY = 'ssh://git.gsc.wustl.edu/srv/git/genome.git';
+our
+our $GIT_REPOS_BASE = $ENV{HOME} . '/.hudson_repos';
+our %REPOSITORIES = (
+	UR => 'git://github.com/sakoht/UR.git',
+	genome => 'ssh://git.gsc.wustl.edu/srv/git/genome.git',
+	workflow => 'ssh://git.gsc.wustl.edu/srv/git/workflow.git',
+);
