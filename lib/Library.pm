f25d90cb6cb0a0bc7b0ef33fb32ff832b0bb5412
Nathan Nutter <nnutter@genome.wustl.edu>
2010-12-29 10:40:29 -0600

    removed unused subs: get_something_hash, get_ur_hash, get_genome_hash, get_workflow_hash, snapshot_namespaces

--- lib/Library.pm
+++ lib/Library.pm
@@ -9,42 +9,6 @@ use UR;
 use LWP::Simple;
 use Defaults;
 
-sub snapshot_namespaces {
-    # create a nice path for working not likely to get blasted
-    my $timestamp = UR::Time->now();
-    $timestamp =~ s/\s/_/g;
-    my $working_path = join('-', 'snapshot-working', $ENV{USER}, $timestamp);
-    $working_path = '/tmp/' . $working_path;
-    system "mkdir $working_path";
-    my $snapshot_path = shift;
-    if (-e $snapshot_path) { die "We don't want to destroy what you've got, so pass a non-existant target path."; }
-    `mkdir $snapshot_path`;
-    `mkdir $snapshot_path/lib`;
-    `mkdir $snapshot_path/lib/perl`;
-    my @repos = @_;
-    my $recorded_hash;
-    my $command;
-    for my $repo (@repos) {
-        $command = "cd $working_path; git clone " . $repo->{'repository_url'} . " " . $repo->{'repository_name'};
-        `$command`;
-        $command = "cd $working_path/" . $repo->{'repository_name'} . "; git show --pretty=oneline " . $repo->{'hash'} . " | tr -d '\n' | cut -d ' ' -f1";
-        $recorded_hash = `$command`;
-        chomp $recorded_hash; chomp $recorded_hash; chomp $recorded_hash;
-        for my $ns ( @{ $repo->{'namespaces'} } ) {
-            print "Beginning work on namespace $ns\n";
-            $command = "cd $working_path/" . $repo->{'repository_name'} . "/" . $repo->{'library_path'} . "; git archive " . $repo->{'hash'} . " $ns | tar -x -C $snapshot_path/lib/perl";
-            `$command`;
-            $command = "cd $working_path/" . "/" . $repo->{'repository_name'} . "/" . $repo->{'library_path'} . "; git archive " . $repo->{'hash'} . " $ns.pm | tar -x -C $snapshot_path/lib/perl";
-            `$command`;
-        }
-        # record hash in revisions.txt
-        $command = 'echo "' . $repo->{'repository_name'} . ' ' . $recorded_hash . '" >> ' . $snapshot_path . "/revisions.txt";
-        `$command`;
-    }
-    $command = "rm $working_path/ -rf";
-    `$command`;
-}
-
 ####
 # Parse Hudson's build status RSS feed and return the most recent successful build from today.
 #
@@ -69,26 +33,6 @@ sub check_for_new_build { # returns new build number or 0 if none.
         }
     }
     return 0;
-}
-
-sub get_workflow_hash { return get_something_hash(shift, "workflow"); }
-
-sub get_genome_hash { return get_something_hash(shift, "genome"); }
-
-sub get_ur_hash { return get_something_hash(shift, "UR"); }
-
-sub get_something_hash {
-    my $build_number = shift;
-    my $something_name = shift;
-    my $revision_txt_path = $Defaults::UNSTABLE_PATH . '/' . $build_number . '/revision.txt';
-    open (revision_fh, $revision_txt_path);
-
-    while (<revision_fh>) {
-        if ( $_ =~ /$something_name/ ) {
-            $_ =~ /$something_name\s(.+)/;
-            return $1;
-        }
-    }
 }
 
 # Shamelessly stolen from Genome/Utility/Text.pm
