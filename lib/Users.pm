989bd42c71b4ad259ab0bcde65975af4067fe6c0
Nathan Nutter <iam@nnutter.com>
2012-07-23 11:38:31 -0500

    explain purpose of apipe and apipe_ignore list

--- lib/Users.pm
+++ lib/Users.pm
@@ -3,6 +3,10 @@ package Users;
 use strict;
 use warnings;
 
+# When someone is removed from LDAP then their email address becomes
+# invalid. If we try to send notifications and their email address is
+# included then the notification just gets dropped without any error.
+# So we have to add people to this ignore list to prevent that.
 sub apipe_ignore {
     return qw(
         adukes
@@ -27,6 +31,10 @@ sub apipe_ignore {
     );
 }
 
+# This list is used to identify "real" APipe members in the sense that
+# we can be sure some APipe members is always responsible for a test
+# failure. This should be the LDAP group minus a few individuals, e.g.
+# ssmith, jeldred, and Systems guys.
 sub apipe { 
     return qw(
         abrummet
