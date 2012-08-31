ec4fa4f24d197659d2655f87f52a57443ba518f3
Nathan Nutter <iam@nnutter.com>
2012-08-31 13:14:19 -0500

    escape subshell

--- lib/Snapshot.pm
+++ lib/Snapshot.pm
@@ -119,8 +119,8 @@ sub create_snapshot_dir {
 	}
 
     # Something to do with generating InlineConfig*. Figure out a better/faster way to generate it.
-    system("cd $snapshot_dir/lib/perl/Genome && /gsc/bin/perl $(which ur) test use");
-    system("cd $snapshot_dir/lib/perl/Genome && /usr/bin/perl $(which ur) test use");
+    system("cd $snapshot_dir/lib/perl/Genome && /gsc/bin/perl \$(which ur) test use");
+    system("cd $snapshot_dir/lib/perl/Genome && /usr/bin/perl \$(which ur) test use");
 	
 	return 1;
 }
