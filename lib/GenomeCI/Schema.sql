63142c1fc9d21a6bb2d081e44fffe59c68d2a34a
Nathan Nutter <iam@nnutter.com>
2013-01-12 00:23:42 -0600

    add username column

--- lib/GenomeCI/Schema.sql
+++ lib/GenomeCI/Schema.sql
@@ -1,1 +1,1 @@
-CREATE TABLE BlessedBuild(model_id INT, perl_version TEXT, git_revision TEXT, PRIMARY KEY(model_id, perl_version));
+CREATE TABLE BlessedBuild(model_id INT, perl_version TEXT, git_revision TEXT, username TEXT, PRIMARY KEY(model_id, perl_version));
