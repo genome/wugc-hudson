fd1b2a6aaa5069209842a06ae5852d4a9c1d352b
apregier <aregier@genome.wustl.edu>
2015-03-26 15:12:08 -0500

    Move common functions to Library

--- lib/Library.pm
+++ lib/Library.pm
@@ -2,6 +2,9 @@ package Library;
 
 use strict;
 use warnings;
+use Revision;
+use JenkinsData;
+use Genome;
 
 BEGIN {
     require Cwd;
@@ -22,6 +25,93 @@ sub log_environment {
     print "\n";
 }
 
+sub set_genome_software_result_test_name {
+    printf("Set GENOME_SOFTWARE_RESULT_TEST_NAME to '%s'.\n", test_version());
+    $ENV{GENOME_SOFTWARE_RESULT_TEST_NAME} = test_version();
+}
+
+sub test_version {
+    my $prefix = $ENV{TEST_VERSION_PREFIX} || '';
+    return $prefix . Revision->test_version();
+}
+
+sub send_timeout_mail {
+    send_mail_with_topic('Timed Out');
+}
+
+sub send_fail_mail {
+    send_mail_with_topic('Build Failed');
+}
+
+sub send_diff_mail {
+    my $diff_cmd = shift;
+
+    send_mail_with_topic('Diffs Found',
+        '********************************************************************************',
+        $diff_cmd->bless_message,
+        '********************************************************************************',
+        $diff_cmd->diffs_message);
+}
+
+sub send_mail_with_topic {
+    my $topic = shift;
+    my @extra_body = @_;
+
+    my ($to, $cc) = get_to_and_cc();
+
+    send_mail(
+        from => 'apipe-tester@genome.wustl.edu',
+        to => $to,
+        cc => $cc,
+        subject => mail_subject($topic),
+        body => mail_body(@extra_body),
+    );
+}
+
+sub mail_subject {
+    my $topic = shift;
+    return sprintf('%s - Build %d - %s', JenkinsData->test_spec,
+        JenkinsData->build_number, $topic);
+}
+
+sub mail_body {
+    return join("\n",
+        sprintf('Project: %s', JenkinsData->job_name),
+        sprintf('Build: %s', JenkinsData->build_url),
+        sprintf('Console: %sconsole', JenkinsData->build_url),
+        @_,
+    );
+}
+
+
+sub get_to_and_cc {
+    if (Genome::Sys->username eq 'apipe-tester') {
+        my $to_default = users_to_addresses(Users::apipe());
+        my $cc_default = users_to_addresses(Users::apipe());
+
+        my $to = email_env('MODEL_TEST_TO', $to_default);
+        my $cc = email_env('MODEL_TEST_CC', $cc_default);
+        return $to, $cc;
+    } else {
+        return Genome::Sys->current_user->email;
+    }
+}
+
+sub email_env {
+    my $key = shift;
+    my $default = shift;
+    if (exists $ENV{$key}) {
+        if ($ENV{$key}) {
+            return $ENV{$key};
+        }
+        else {
+            return;
+        }
+    }
+    else {
+        return $default;
+    }
+}
 # Shamelessly stolen from Genome/Utility/Text.pm
 sub model_class_name_to_string {
     my $type = shift;
