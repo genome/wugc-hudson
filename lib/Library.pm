cc41cf7c36962950d9b4f9c925210a72cd7e6586
apregier <aregier@genome.wustl.edu>
2015-08-05 12:36:46 -0500

    Revert "Move setup and diffs into genome space"
    This reverts commit 861c8a90846dc8630b72411041c6eedda7aac63f.

--- lib/Library.pm
+++ lib/Library.pm
@@ -173,7 +173,7 @@ sub setup_model_process_test {
 
 sub diff_cmd_has_diffs {
     my $diff_cmd = shift;
-    return $diff_cmd->has_diffs;
+    return (defined($diff_cmd->_diffs) && scalar(keys %{$diff_cmd->_diffs})) || 0;
 }
 
 sub wait_for_build {
